f0d9f74f6c29ac26d3cc855586d4148d06d78d4d
thidspessoa <thiagopessoa246@gmail.com>
2026-02-01 16:02:53 -0300

    refactor(all): Ajusta a estrutura do projeto, implementando Separation of Concerns

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -129,9 +129,9 @@ const Hero: React.FC = () => {
       </div>
       
       {/* Scroll indicator */}
-      <div className="absolute bottom-10 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 opacity-30">
+      <div className="absolute bottom-10 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 opacity-30 animate-bounce">
         <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center p-1">
-          <div className="w-1 h-2 bg-white rounded-full animate-bounce" />
+          <div className="w-1 h-2 bg-white rounded-full" />
         </div>
       </div>
     </section>
